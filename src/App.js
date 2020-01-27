bd197312d4eb5e44be7aa5c172933ace7dbdf479
kingdayx <54965488+kingdayx@users.noreply.github.com>
2020-01-27 05:38:28 -0500

    added scoreboard and css

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <header>
+        <h1>Scoreboard</h1>
+        <span className="stats">PLayers: 1</span>
+        <div classNmae="player">
+      <span className="player-name">
+        Guil
+      </span>
+      <div className="counter">
+        <button className="counter-action decrement"> - </button>
+        <span className="counter-score">35</span>
+        <button className="counter-action decrement"> + </button>
     </div>
+    </div>
+
+    </header>
   );
 }
 
+const Player = () => {
+return (
+    <div classNmae="player">
+      <span className="player-name">
+        Guil
+      </span>
+<div className="counter">
+  <button className="counter-action decrement"> - </button>
+  <span className="counter-score">35</span>
+  <button className="counter-action decrement"> + </button>
+</div>
+    </div>
+
+);
+
+}
+
 export default App;
+
+
